655df5b12a4366ede99df01a53231308113d8a34
ilyav11 <vershkov.ml@gmail.com>
2018-08-12 16:56:26 +0300

    First commit

---
+++ consistent.py
@@ -0,0 +1,24 @@
+import ipaddress
+
+
+class Nexthop :
+
+    def __init__(self, ipaddr):
+        self._ipaddr = ipaddr
+
+
+class ActualContainer:
+    def __init__(self):
+        pass
+
+
+class DesiredContainer:
+    def __init(self):
+        pass
+
+
+class Route:
+    def __init(self):
+        pass
+
+
